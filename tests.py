9c4e07fcfce29eda3cbf91957ca150e6254b26e1
Kristaps Kulis <Kristaps.Kulis@gmail.com>
2011-05-26 00:05:12 +0300

    basic behaviour tests

--- tests.py
+++ tests.py
@@ -18,11 +18,33 @@ class BrokerTest(unittest.TestCase):
         self.assertEqual(data, policy)
         client.close()
     def test_channel_one_on_one_communication(self):
-        client_first = socket.create_connection("127.0.0.1", 843)
-        client_second = socket.create_connection("127.0.0.1", 843)
-        client_first.send("broker send second")
-        status, junk = client_first.recv(1024)
+        client_first = socket.create_connection(("127.0.0.1", 1001))
+        client_second = socket.create_connection(("127.0.0.1", 1001))
+        client_first.send("broker send second\r\n")
+        client_second.send("broker receive second\r\n")
+        status, junk = client_first.recv(256).split(":")
+        status_second, junk = client_second.recv(256).split(":")
+
         self.assertEqual(status, "SUCCESS")
+        self.assertEqual(status_second, "SUCCESS")
+        client_first.close()
+        client_second.close()
+    def test_one_to_many_communication(self):
+        broadcaster = socket.create_connection(("127.0.0.1", 1001))
+        broadcaster.send("broker send broadcast\r\n")
+        data, junk = broadcaster.recv(512).split(":")
+        self.assertEqual(data, "SUCCESS")
+        clients = []
+        for i in range(0, 20):
+            client = socket.create_connection(("127.0.0.1", 1001))
+            client.send("broker receive broadcast\r\n")
+            data, junk = client.recv(512).split(":")
+            self.assertEqual(data, "SUCCESS")
+            clients.append(client)
+        broadcaster.send("broadcast\r\n")
+        for client in clients:
+            data = client.recv(512)
+            self.assertEqual(data, "broadcast\r\n")
 
     def tearDown(self):
         self.process.terminate()
