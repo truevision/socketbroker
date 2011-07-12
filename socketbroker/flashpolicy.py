77397a39615cf88703086919a3b72fedb94e4fd1
Kristaps Kulis <kristaps.kulis@gmail.com>
2011-07-12 23:14:23 +0300

    Rewriting all from asyncore to threading SocketServer

--- socketbroker/flashpolicy.py
+++ socketbroker/flashpolicy.py
@@ -1,52 +1,40 @@
-import asyncore
-import asynchat
 import logging 
-import socket 
+import threading
+import SocketServer 
 
-MAX_DATA_LENGTH = 1024
-
-class RemoteClient(asynchat.async_chat):
-    def __init__(self, socket, port = "*"):
-        asynchat.async_chat.__init__(self, socket)
-        self.port = port
-        self.set_terminator("\x00")
-        self.data = ""
+class FlashPolicyTCPHandler(SocketServer.BaseRequestHandler):
+    """ 
+        flash policy TCP socket handler
+        returns flash policy XML, which allows to connect to allowed ports
+    """
+    def setup(self):
         self.log = logging.getLogger("FlashPolicy.Client")
-        self.log.info("new client")
-    def collect_incoming_data(self, data):
-        self.log.debug("recieved data %s" % data)
-        total_len = len(data) + len(self.data)
-        if total_len > MAX_DATA_LENGTH:
-            self.push("too much data. closing connection")
-            self.log.warn("too much data received (%d bytes)", total_len)
-            self.handle_close()
-            return 
+        self.log.debug("setup")
+    def handle(self):
 
-        self.data = self.data + data 
-    def found_terminator(self):
-        self.log.debug("found terminator")
-        if 'policy-file-request' not in self.data.lower().strip():
-            self.push("invalid request")
-            self.log.warn("invalid request (%s)" % self.data)
-            self.handle_close()
-        policy = '<cross-domain-policy><allow-access-from domain="*", ports="%s" /></cross-domain-policy>' % (self.port)
-        self.push(policy)
-        self.log.info("pushing policy ")
-    def handle_close(self):
-        asynchat.async_chat.handle_close(self)
-        self.log.info("closing connection")
+        self.log.debug("new client from %s" % self.client_address[0])
+        data = self.request.recv(128)
+        self.log.debug("received data %s " % data)
+        if data.endswith("\x00") and "flash-policy-request" in data.lower():
+            self.log.info("sending policy file for port %s" % self.server.port )
+            self.request.send('<cross-domain-policy><allow-access-from domain="*", ports="%s" /></cross-domain-policy>' % self.server.port)
+        else:
+            self.log.warn("invalid request")
+            self.request.send("<error>invalid request</error>")
+    def finish(self):
+        self.log.debug("finished request")
 
-class PolicyDispatcher(asyncore.dispatcher):
-    def __init__(self, ip = "0.0.0.0", dest_port = "*"):
-        self.dest_port = dest_port
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.log = logging.getLogger("FlashPolicy")
-        self.set_reuse_addr()
-        self.log.info("binding to %s:%s" % (ip, 843))
-        self.bind((ip, 843))
-        self.listen(5)
-    def handle_accept(self):
-        socket, address = self.accept()
-        RemoteClient(socket, self.dest_port)
-    
+class FlashPolicyTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+def start(ip, dest_port):
+    logger = logging.getLogger("FlashPolicy.Server")
+    logger.debug("creating server")
+    server = FlashPolicyTCPServer((ip, 843), FlashPolicyTCPHandler)
+    server.port = dest_port
+    logger.debug("creating server thread")
+    server_thread = threading.Thread(target = server.serve_forever)
+    server_thread.setDaemon(True)
+    server_thread.start()
+    logger.debug("server runnning in thread %s ", server_thread.getName())
+    return server    
